2b512e374b6ad1add4d69cd9b2a139017df976fa
s.gamma@spxlab.com <sm4rtl4nd>
2021-03-28 23:15:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="bottomColor" />
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
